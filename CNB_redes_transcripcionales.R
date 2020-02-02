fa4304a3433e5fa7ec3e0f3ed491b860399bbf9a
Jorge Domínguez <jodombar@gmail.com>
2020-02-02 11:01:38 +0100

    update

--- CNB_redes_transcripcionales.R
+++ CNB_redes_transcripcionales.R
@@ -1,12 +1,14 @@
-
-
-
+#################################################################
+######        ANÁLISIS DE REDES TRANSCRIPCIONALES         #######
+####  Autores: Sara Cartan Moya | Jorge Domínguez Barragán   ####
+###  Contacto: saracartanmoya@gmail.com | jodombar@gmail.com  ###
+#################################################################
 
 ## Las redes transcripcionales son redes dirigidas donde los nodos representan genes 
 ## y se traza una arista de un nodo g1 al node g2 cuando g1 codifica por un factor de 
-## transcripci?n se une al promoter del g2. T?picamente, est?s redes se construyen a 
+## transcripción se une al promoter del g2. Típicamente, estas redes se construyen a 
 ## partir de datos de ChIP-seq y suponen un refinameinto de la redes de co-expresi?n 
-## g?nica. 
+## génica. 
 
 ## Este script recibe como entrada un fichero donde a su vez se guardan los 
 ## identificadores de los genes para los cuales a partir de datos de ChIP-seq se han 
@@ -34,7 +36,7 @@ length(genes.in.network)
 
 ## Generamos incialmente una matriz de adyacencia con todos los valores 0 y vamos
 ## rellenándola: los valores 1 son las dianas de los correspondientes factores
-## de transcripci?n. 
+## de transcripción. 
 ad.matrix<- matrix(data=0, 
                    nrow = length(genes.in.network), 
                    ncol = length(genes.in.network))
@@ -63,14 +65,9 @@ write.graph(gene.transcriptional.network,file="transcriptional_gene_network.gml"
 
 gene.transcriptional.network
 
-##AQUÍ SE GENERA LA RED TRANSCRIPCIONAL PERO NO ES LA QUE HAY QUE USAR EN EL CYTOSCAPE
-
 ## Esta red es excesivamente grande y por motivos de recursos computacionales y tiempo
 ## vamos a trabajar s?lo con la red inducida que contiene los factores de transcripci?n
 ## y las aristas entre ellos.
-
-##AQUÍ SÍ SE GENERA LA RED TRANSCRIPCIONAL QUE HAY QUE USAR EN EL CYTOSCAPE.
-##CUANDO ESO ESTÉ SI QUIERES ME LO PASAS Y LO HAGO A VER SI ME VA
 
 tfs.network <- induced.subgraph(gene.transcriptional.network,tf.name)
 plot.igraph(x = tfs.network,vertex.size=8,edge.arrow.size=0.5,vertex.label="",vertex.color="blue")
@@ -105,16 +102,13 @@ log10.node.degrees <- log10(as.numeric(names(degree.frequencies)))
 lm.r <- lm(log10.degrees.frequencies ~ log10.node.degrees)
 summary(lm.r)
 
-##EN EL SUMMARY HAY QUE VER EL VALOR DEL P-VALOR. Si es menor que 0.05 es libre de escala
-## El p-valor sale 0.9898, NO es libre de escala
+## El p-valor sale 0.9898, por tanto, nO es libre de escala
 
 ## Las redes aleatorias que cumplen las mismas propiedas que nuestra
 ## red de interés son las generadas seg?n el modelo de Erdos-Renyi. La funci?n
 ## erdos.renyi.game genera redes aleatorias que siguen una distribuci?n de Poisson
 ## con un n?mero de nodos y aristas dado. 
 tfs.network
-
-##AQUÍ TIENES QUE METER EL NÚMERO DE NODOS EN EL n (EL PRIMER NÚMERO QUE SALE)
 
 random.graph <- erdos.renyi.game(n=10, p.or.m=22, type="gnm", directed=TRUE, loops=TRUE)
 plot.igraph(x = random.graph,vertex.size=8,edge.arrow.size=0.5,vertex.label="",vertex.color="blue")
@@ -132,7 +126,7 @@ autorregulation.in.tfs
 
 ## N?mero de genes autorregulados en la red 
 autorregulation.in.random <- sum(diag(as.matrix(get.adjacency(random.graph))))
-autorregulation.in.random ##•ESTE ES UN NÚMERO QUE SE GENERA DE FORMA ALEATORIA
+autorregulation.in.random 
 
 ## Para comprobar la signficancia de la autorregulaci?n generamos 1000 redes
 ## aleatorias y calculamos el n?mero de autorregulaciones en cada una almacen?ndola
@@ -148,11 +142,7 @@ for (i in 1:10000)
 mean(autorregulation.random.graphs)
 sd(autorregulation.random.graphs)
 
-sum(autorregulation.random.graphs > autorregulation.in.tfs)/10000 ##El p.valor es de 10^-4 que es el numero de veces que he generado
-
-##AQUÍ AL FINAL LO QUE SE MIRA ES LA RELEVANCIA DE LOS MOTIVOS AUTORREGULADOS QUE DEPENDE
-##DEL VALOR QUE SALGA EN LAS REDES GENERADAS AUTOMÁTICAMENTE
-
+sum(autorregulation.random.graphs > autorregulation.in.tfs)/10000 
 
 ## Motivos de red de tres nodos
 
@@ -168,10 +158,6 @@ length(occurrency.subgraph.three.nodes)
 
 ## La funci?n graph.isocreate genera todos los grafos posibles de un tama?o dado
 ## empezando a enumerarlos por 0. 
-
-##LOS GRAFOS SON LOS DIFERENTES MOTIVOS QUE DIMOS EL AÑO PASADO (EN PLAN QUE UN REGULA A OTRO Y TAL)
-##ENTONCES EXISTEN 16 DIFERENTES Y NOSOTROS TENEMOS QUE MIRAR LOS QUE SALEN (EN PLAN SI SALE NA ES QUE NO 
-##APARECE NINGUNA VEZ)
 
 plot.igraph(graph.isocreate(size=3, number=4))
 plot.igraph(graph.isocreate(size=3, number=6))
@@ -189,7 +175,6 @@ plot.igraph(graph.isocreate(size=3, number=13))
 motifs.3.random.graph <- matrix(0,nrow=1000, ncol=16)
 motifs.3.random.graph[1:3,]
 
-##AQUÍ CAMBIA EL N
 for (i in 1:1000)
 {
   random.graph <- erdos.renyi.game(n=10, p.or.m=22, type="gnm", directed=TRUE, loops=TRUE)
@@ -198,8 +183,9 @@ for (i in 1:1000)
 
 motifs.3.random.graph[1:3,]
 
-##ESTAMOS GENERANDO REDES ALEATIRUAS PARAVER SI ES MOTIVO DE RED
-
+###################################################
+####       ESTUDIO DE MOTIVOS DE RED         ######
+###################################################
 ## subgrafo 5
 plot(graph.isocreate(size=3, number=4))
 occurrency.subgraph.three.nodes[5]
@@ -207,8 +193,6 @@ occurrency.subgraph.three.nodes[5]
 mean(motifs.3.random.graph[,5])
 sd(motifs.3.random.graph[,5])
 sum(motifs.3.random.graph[,5] >1)/1000
-
-##No significativo pq el p.valor que yo he generado es de 10^-4 (en el ejemplo de fran)
 
 ## subgrafo 7
 plot(graph.isocreate(size=3, number=6))
@@ -261,14 +245,4 @@ sum(motifs.3.random.graph[,14] > 1)/1000
 ## Los subgrafos que son motivos con interés biológico son el 7 y el 10
 
 subgrafo.7 <- graph.isocreate(size=3, number=6)
-
-## Para encontrar todas las instancias de este subgrafo en nuestra red usamos 
-## la siguiente instrucci?n. 
-maps.subgrafo.7 <- graph.subisomorphic.lad(subgrafo.7, tfs.network, all.maps=TRUE)[["maps"]]
-maps.subgrafo.7
-
-##ESTO DICE FERNANDO QUE ES QUE TE DICE DÓNDE ESTÁN LOS SUBGRAFOS EN LA RED
-
 subgrafo.10<- graph.isocreate(size = 3, number = 9)
-maps.subgrafo.10<- graph.subisomorphic.lad(subgrafo.10, tfs.network, all.maps = TRUE)[["maps"]]
-maps.subgrafo.10
